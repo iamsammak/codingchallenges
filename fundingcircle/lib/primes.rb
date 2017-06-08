7eb14864d4a1c7b9779f12b09cd507b630662e52
Samuel Mak <samuelswmak@gmail.com>
2017-06-07 23:33:06 -0700

    add gemfile

--- fundingcircle/lib/primes.rb
+++ fundingcircle/lib/primes.rb
@@ -1,1 +1,25 @@
-test
+require "byebug"
+
+def is_prime?(num)
+  return false if num < 2
+
+  i = 2
+  until i == num/2
+    return false if num % i == 0
+    i += 1
+  end
+  true
+end
+
+#
+# def primes(num)
+#   ret = []
+#
+#   i = 0
+#   while ret.length < num
+#     ret << i if is_prime?(i)
+#     i += 1
+#   end
+#
+#   ret
+# end
