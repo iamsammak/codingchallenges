182f61906ee8e2c0f050e162b9360e71e11e3c9f
Samuel Mak <samuelswmak@gmail.com>
2017-06-12 22:22:47 -0700

    complete primes multiplication table

--- fundingcircle/lib/primes.rb
+++ fundingcircle/lib/primes.rb
@@ -1,10 +1,8 @@
 require "byebug"
 require 'terminal-table'
 
-
 def is_prime?(num)
   return false if num < 2
-  return true if num == 2
 
   i = 2
   until i == num
@@ -28,19 +26,38 @@ def primes(n)
 end
 
 def generate_table(n)
-  p_table = Array.new(n) { Array.new }
-  p_table.unshift(primes(n).unshift(nil)) # unshift nil for an empty space
+  p_table = Array.new(n) { [] }
+  # unshift nil for an empty space
+  p_table.unshift(primes(n).unshift(nil))
+
+  # create first col
   p_table.each_with_index do |arr, idx|
     next if idx == 0 # skip empty space
-    arr.push(primes_table[0][idx])
+    arr.push(p_table[0][idx])
   end
 
+  # seed the table
+  (1...p_table.length).each do |col|
+    (col...p_table.length).each do |row|
+      product = p_table[0][col] * p_table[row][0]
+      p_table[row][col] = product
+      p_table[col][row] = product
+    end
+  end
+  p_table
 end
 
 def print_table(array)
   table =
-    Terminal::Table.new :title => "Prime Number Multiplication Table",
-                        :headings => array.shift,
-                        :rows => array
+    Terminal::Table.new title: "Prime Number Multiplication Table",
+                        headings: array.shift,
+                        rows: array
   puts table
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts "Generating Prime Number Multiplication Table"
+  puts "How many prime numbers would you like?"
+  answer = gets.chomp.to_i
+  print_table(generate_table(answer))
 end
--- fundingcircle/spec/primes_spec.rb
+++ fundingcircle/spec/primes_spec.rb
@@ -2,27 +2,27 @@ require 'rspec'
 require 'primes'
 
 describe "#is_prime?" do
-  it "returns false if n == 1" do
+  it "false if n == 1" do
     expect(is_prime?(1)).to be false
   end
 
-  it "returns false if n == 0" do
+  it "false if n == 0" do
     expect(is_prime?(0)).to be false
   end
 
-  it "returns true if n == 2 " do
+  it "true if n == 2 " do
     expect(is_prime?(2)).to be true
   end
 
-  it "returns false if n is negative" do
-    expect(is_prime?(-1)).to be false
-  end
-
-  it "returns true if n is prime" do
+  it "true if n is prime" do
     expect(is_prime?(3)).to be true
   end
 
-  it "returns false if n is not prime" do
+  it "false if n is not prime" do
     expect(is_prime?(4)).to be false
+  end
+
+  it "false if n is negative" do
+    expect(is_prime?(-1)).to be false
   end
 end
